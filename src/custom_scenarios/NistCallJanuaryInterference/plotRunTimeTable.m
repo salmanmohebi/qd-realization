adae6652cb51635273d82d1363fca738781424db
Mattia Lecci <mattia.lecci@gmail.com>
2020-01-14 18:41:13 +0100

    Add runAllConfigParams and plotRunTimeTable to interf. scenario

---
+++ src/custom_scenarios/NistCallJanuaryInterference/plotRunTimeTable.m
@@ -0,0 +1,15 @@
+clear
+close all
+clc
+
+%%
+load('runTimeTable3')
+
+for i = 1:size(runTimeTable, 2)-1
+    figure
+    boxplot(runTimeTable{:, end}, runTimeTable{:, i})
+    xlabel(runTimeTable.Properties.VariableNames{i})
+    ylabel('Run-time [s]')
+    grid on
+    grid minor
+end
