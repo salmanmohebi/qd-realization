c7345e04c3a015456ea38a3b31d13f499eb7b0c6
Mattia Lecci <mattia.lecci@gmail.com>
2020-03-13 12:00:22 +0100

    Add launchApplyQd
    Moving from main to a separate function the post-processing that applies the QD model to a given scenario

--- src/mainApplyQd.m
+++ src/mainApplyQd.m
@@ -4,40 +4,7 @@ clc
 
 addpath('utils', 'raytracer')
 %%
-scenario = 'ScenarioTest';
+scenarioPath = 'ScenarioTest';
 qdGeneratorFunc = @completeMultipleReflectionQdGenerator;
 
-% Import Output files
-qdFiles = readAllQdFiles(scenario);
-triangLists = readAllTriangLists(scenario);
-nodesPosition = readAllNodesPositions(scenario);
-
-paramCfg = parameterCfg(scenario);
-
-% Import material library
-% Working directory might be different, reset it to src/
-matLibraryPath = paramCfg.materialLibraryPath;
-startPathIdx = strfind(matLibraryPath, 'material_libraries/');
-matLibraryPath = matLibraryPath(startPathIdx:end);
-
-materialLibrary = importMaterialLibrary(matLibraryPath);
-
-% Import cadData
-[cadData, switchMaterial] = getCadOutput(...
-    paramCfg.environmentFileName,...
-    fullfile(scenario, 'Input'),...
-    materialLibrary,...
-    paramCfg.referencePoint,...
-    paramCfg.selectPlanesByDist,...
-    paramCfg.indoorSwitch);
-
-%% post-processing
-if switchMaterial
-    qdFilesOut = applyQd(qdFiles, triangLists, nodesPosition, cadData,...
-        materialLibrary, paramCfg, qdGeneratorFunc);
-end
-
-%% write to file
-qdFolderPath = fullfile(scenario, 'Output/Ns3/QdFiles_QD');
-mkdir(qdFolderPath)
-writeAllQdFiles(qdFilesOut, qdFolderPath, 6);
+launchApplyQd(scenarioPath, 'qdGeneratorFunc', qdGeneratorFunc);
